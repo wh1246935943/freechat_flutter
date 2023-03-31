0ab1b5366add973fd1d798d9a12bf4a26e0ee1b4
wh1246935943 <1246935943@qq.com>
2023-03-31 18:16:44 +0800

    最近会话页面开发

---
+++ lib/pages/Chat/__index.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:freechat/pages/Chat/SessionBox/index.dart';
+import 'package:freechat/pages/common/NavbarMoreList/index.dart';
+
+class Chat extends StatefulWidget {
+  const Chat({super.key});
+
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  GlobalKey iconAddkey = GlobalKey();
+  late double iconAddOffsetX;
+  late double iconAddOffsetY;
+  late Size iconAddSize;
+  late Function closeModel;
+
+  final List<String> _chatUserList = <String>['王浩', '测试1', '测试2'];
+
+  void _searchContent() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          centerTitle: true,
+          title: const Text('免聊'),
+          actions: <Widget>[
+            IconButton(
+                icon: const Icon(Icons.search), onPressed: _searchContent),
+            IconButton(
+                key: iconAddkey,
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  // 通过key获取到widget的位置
+                  RenderObject addNode = iconAddkey.currentContext?.findRenderObject() as RenderObject;
+
+                  // 获取widget的高宽
+                  iconAddSize = addNode.paintBounds.size;
+
+                  // 获取位置
+                  final temp = addNode.getTransformTo(null).getTranslation();
+
+                  iconAddOffsetX = temp.x;
+                  iconAddOffsetY = temp.y;
+                  
+                  _moreActions(context);
+                }),
+          ],
+        ),
+        body: _buildChatList());
+  }
+
+  Widget _buildChatList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(0.0),
+      itemBuilder: (BuildContext context, int i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
+        final int index = i ~/ 2;
+        if (index >= _chatUserList.length) {
+          _chatUserList.addAll(
+              ['王浩', '测试1', '测试2', '王浩', '测试1', '测试2', '王浩', '测试1', '测试2']);
+        }
+        return _buildRow(_chatUserList[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(String name) {
+    return ListTile(
+      title: Text(name),
+      trailing: const Text('23:10'),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute<void>(
+              builder: (context) => const SessionBox(),
+              settings: RouteSettings(
+                name: name,
+              )),
+        );
+      },
+    );
+  }
+
+  void _moreActions(BuildContext context) {
+
+    final left = iconAddOffsetX;
+    final top = iconAddOffsetY + iconAddSize.height;
+
+    Navigator.push(context,
+      NavbarMoreList(
+        left: left,
+        top: top
+      )
+    );
+  }
+}
--- lib/pages/Chat/index.dart
+++ lib/pages/Chat/index.dart
@@ -1,88 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:freechat/pages/common/dd_image/dd_image.dart';
 import 'package:freechat/pages/Chat/SessionBox/index.dart';
 import 'package:freechat/pages/common/NavbarMoreList/index.dart';
 
 class Chat extends StatefulWidget {
   const Chat({super.key});
-
   @override
   State<Chat> createState() => _ChatState();
 }
 
 class _ChatState extends State<Chat> {
+
   GlobalKey iconAddkey = GlobalKey();
   late double iconAddOffsetX;
   late double iconAddOffsetY;
   late Size iconAddSize;
-  late Function closeModel;
-
-  final List<String> _chatUserList = <String>['王浩', '测试1', '测试2'];
 
   void _searchContent() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 1,
-          centerTitle: true,
-          title: const Text('免聊'),
-          actions: <Widget>[
-            IconButton(
-                icon: const Icon(Icons.search), onPressed: _searchContent),
-            IconButton(
-                key: iconAddkey,
-                icon: const Icon(Icons.add),
-                onPressed: () {
-                  // 通过key获取到widget的位置
-                  RenderObject addNode = iconAddkey.currentContext?.findRenderObject() as RenderObject;
+      appBar: AppBar(
+        title: const Text('免聊'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search), onPressed: _searchContent),
+          IconButton(
+            key: iconAddkey,
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              // 通过key获取到widget的位置
+              RenderObject addNode = iconAddkey.currentContext?.findRenderObject() as RenderObject;
 
-                  // 获取widget的高宽
-                  iconAddSize = addNode.paintBounds.size;
+              // 获取widget的高宽
+              iconAddSize = addNode.paintBounds.size;
 
-                  // 获取位置
-                  final temp = addNode.getTransformTo(null).getTranslation();
+              // 获取位置
+              final temp = addNode.getTransformTo(null).getTranslation();
 
-                  iconAddOffsetX = temp.x;
-                  iconAddOffsetY = temp.y;
-                  
-                  _moreActions(context);
-                }),
-          ],
-        ),
-        body: _buildChatList());
-  }
-
-  Widget _buildChatList() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(0.0),
-      itemBuilder: (BuildContext context, int i) {
-        if (i.isOdd) {
-          return const Divider();
+              iconAddOffsetX = temp.x;
+              iconAddOffsetY = temp.y;
+              
+              _moreActions(context);
+            }
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: 20, // 假设有20个会话
+        itemBuilder: (BuildContext context, int index) {
+          return ConversationItem(
+            avatar: 'assets/images/avatar_$index.jpg', // 假设有20张头像图片
+            title: '联系人 $index',
+            message: '最近的聊天记录',
+            time: '下午1:23',
+            unreadCount: index % 3, // 假设未读消息数量为0~2之间的随机数
+          );
         }
-        final int index = i ~/ 2;
-        if (index >= _chatUserList.length) {
-          _chatUserList.addAll(
-              ['王浩', '测试1', '测试2', '王浩', '测试1', '测试2', '王浩', '测试1', '测试2']);
-        }
-        return _buildRow(_chatUserList[index]);
-      }
-    );
-  }
-
-  Widget _buildRow(String name) {
-    return ListTile(
-      title: Text(name),
-      trailing: const Text('23:10'),
-      onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute<void>(
-              builder: (context) => const SessionBox(),
-              settings: RouteSettings(
-                name: name,
-              )),
-        );
-      },
+      )
     );
   }
 
@@ -96,6 +73,67 @@ class _ChatState extends State<Chat> {
         left: left,
         top: top
       )
+    );
+  }
+}
+
+class ConversationItem extends StatelessWidget {
+  final String avatar;
+  final String title;
+  final String message;
+  final String time;
+  final int unreadCount;
+
+  const ConversationItem({
+    super.key, 
+    required this.avatar,
+    required this.title,
+    required this.message,
+    required this.time,
+    required this.unreadCount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const DDImage(
+        url:'http://freechat.bilinstore.com/files/avatar/6ab4b259-2838-4620-ad73-a3e74539ffb01674267717151.png',
+        width: 40,
+        height: 40,
+      ),
+      title: Text(title),
+      subtitle: Text(message),
+      trailing: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text(time),
+          if (unreadCount > 0)
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(
+                '$unreadCount',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
+              ),
+            ),
+        ],
+      ),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute<void>(
+            builder: (context) => const SessionBox(),
+            settings: RouteSettings(
+              name: title,
+            )
+          )
+        );
+      },
     );
   }
 }
